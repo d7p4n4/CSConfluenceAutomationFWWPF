91b8bd5ac5c7d930aa8ffb79cd1fdfa37912fa25
Zsombor <zsombor.berces@artanacsado.hu>
2020-02-03 15:57:19 +0100

    Add project files.

---
+++ CSConfluenceAutomationFWWPF/MainWindow.xaml.cs
@@ -0,0 +1,84 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CSConfluenceAutomationFWWPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public string APPSETTINGS_JELSZO = ConfigurationManager.AppSettings["Jelszo"];
+        public string APPSETTINGS_FELHASZNALONEV = ConfigurationManager.AppSettings["FelhasznaloNev"];
+        public string APPSETTINGS_URL = ConfigurationManager.AppSettings["URL"];
+        public string APPSETTINGS_TERAZONOSITO = ConfigurationManager.AppSettings["TerAzonosito"];
+        public string APPSETTINGS_SZULOOSZTALYAZONOSITO = ConfigurationManager.AppSettings["SzuloOsztalyAzonosito"];
+        public string APPSETTINGS_OLDALCIM = ConfigurationManager.AppSettings["OldalCim"];
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        /*-
+        private string btnOpenFile_Click(object sender, RoutedEventArgs e)
+        {
+            string html = "";
+
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "html files (*.html)|*.html";
+            openFileDialog.InitialDirectory = @"d:\";
+
+            if (openFileDialog.ShowDialog() == true)
+            {
+                html = File.ReadAllText(openFileDialog.FileName);
+            }
+
+            return html;
+        }
+        */
+        private void UploadClick(object sender, RoutedEventArgs e)
+        {
+            Metodus metodus = new Metodus();
+            
+            string html = "";
+
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "html files (*.html)|*.html";
+            openFileDialog.InitialDirectory = @"d:\";
+
+            if (openFileDialog.ShowDialog() == true)
+            {
+                html = File.ReadAllText(openFileDialog.FileName);
+            }
+
+            Regex re = new Regex("\r(?= *\r)");
+
+            html = html.Replace("\r", "").Replace("\n", "").Replace("\t", "").Replace("\"", "'");
+
+            metodus.AddConfluencePage(
+                APPSETTINGS_OLDALCIM
+                , APPSETTINGS_TERAZONOSITO
+                , APPSETTINGS_SZULOOSZTALYAZONOSITO
+                , html
+                , APPSETTINGS_URL
+                , APPSETTINGS_FELHASZNALONEV
+                , APPSETTINGS_JELSZO
+                );
+       }
+    }
+}
---
+++ CSConfluenceAutomationFWWPF/Metodus.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSConfluenceAutomationFWWPF
+{
+    public class Metodus
+    {
+        public void AddConfluencePage(string cim, string terAzonosito, string szuloOsztalyAzonosito, string html, string URL, string felhasznaloNev, string jelszo)
+       {
+            //string DATA = @"{""type"":""page"",""title"":" + cim + @",""ancestor"":[{""type"":""page"",""id:" + szuloOsztalyAzonosito +
+            //    @"}],""space"":{""key"":" + terAzonosito + @",""body"":{""storage"":{""value"":" + html + @",""representation"":""storage""}}}";
+
+
+             string DATA = "{\"type\":\"page\",\"title\":\"" + cim + "\",\"space\":{\"key\":\"" + terAzonosito + "\"},\"body\":{\"storage\":{\"value\":\"" + html + "\",\"representation\":\"storage\"}}}";
+
+        System.Net.Http.HttpClient client = new System.Net.Http.HttpClient();
+            client.BaseAddress = new System.Uri(URL);
+            byte[] cred = UTF8Encoding.UTF8.GetBytes(felhasznaloNev + ":" + jelszo);
+            client.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Basic", Convert.ToBase64String(cred));
+            client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+
+            System.Net.Http.HttpContent content = new StringContent(DATA, UTF8Encoding.UTF8, "application/json");
+
+            HttpResponseMessage messge = client.PostAsync(URL, content).Result;
+            string description = string.Empty;
+            if (messge.IsSuccessStatusCode)
+            {
+               string result = messge.Content.ReadAsStringAsync().Result;
+                description = result;
+            }
+        }
+    }
+}
